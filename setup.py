701465ed6332569cebb9ad5b1a0f1e0e3b21ce08
Andreas Jung <info@zopyx.com>
2020-07-13 11:32:53 +0200

    0.2.2 release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = '0.2.1'
+version = '0.2.2'
 
 long_description = (
         read('README.rst')
