8f7374d80b7d51156b590935c43e09d28800b20d
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-11-07 14:03:40 +0100

    Back to development: 0.3.3
    [ci skip]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = '0.3.2'
+version = '0.3.3.dev0'
 
 long_description = (
         read('README.rst')
