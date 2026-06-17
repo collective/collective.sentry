cd060a09a2d0bf3d4e3c7dea5259fd61a164e54e
Gil Forcada Codinachs <gil.gnome@gmail.com>
2026-06-17 15:26:15 +0200

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = "1.0.0"
+version = "1.0.1.dev0"
 
 long_description = (
     read("README.rst") + "\n" + read("CHANGES.txt") + "\n" + read("CONTRIBUTORS.txt")
